4a4320b9dd85c689f0cdf88c1590dc29c9abc293
Endelkys <endelkysanelmar2604@gmail.com>
2024-06-08 20:56:45 -0400

    Configuracion inical del servidor

---
+++ backend/app.js
@@ -0,0 +1,51 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const cors = require('cors');
+
+var db = require('./connection/db');
+
+var indexRouter = require('./routes/index');
+var productosRouter = require('./routes/productos.r');
+var usuariosRouter = require('./routes/usuarios.r');
+var registrarRouter = require('./routes/registrar.r');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/productos', productosRouter);
+app.use('/usuarios', usuariosRouter);
+app.use('/registrar', registrarRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+db();
+
+module.exports = app;
